88489d6b066fe48987b7e57d8cf102695c815bbe
datsun80zx <m.richardp93@gmail.com>
2025-01-09 08:48:02 -0500

    added movement

--- main.py
+++ main.py
@@ -9,23 +9,24 @@ def main():
     dt = 0
     X = SCREEN_WIDTH / 2 
     Y = SCREEN_HEIGHT / 2 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player_1 = Player(X, Y)
-     
+    
+
     while True:
        for event in pygame.event.get():
            if event.type == pygame.QUIT:
                return
        screen.fill((0, 0, 0))
-       player_1.update(dt)
-       player_1.draw(screen)
+       for player in updatable:
+          player.update(dt)
+       
+       for player in drawable:
+           player.draw(screen)
        pygame.display.flip()
        dt = (clock.tick(60)) / 1000
-
-    
-
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__=="__main__":
     main()
