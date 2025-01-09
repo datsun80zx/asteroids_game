28ed2101922812ed828ad5662a6de8f57ffce0f3
datsun80zx <m.richardp93@gmail.com>
2025-01-09 10:50:36 -0500

    missed file save

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,5 @@
+import pygame
 from circleshape import *
-from constants import * 
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
--- circleshape.py
+++ circleshape.py
@@ -22,8 +22,6 @@ class CircleShape(pygame.sprite.Sprite):
         pass
 
     def collision_check(self, other):
-        distance = self.position.distance_to(other.position)
-        if distance <= self.radius + other.radius:
-            return True
-        return False
+        return self.position.distance_to(other.position) <= self.radius + other.radius
+    
     
