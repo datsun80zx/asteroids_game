dab987d8cd614c97ffa17adc726fc4d976dc62ba
datsun80zx <m.richardp93@gmail.com>
2025-01-09 07:56:50 -0500

    created the player triangle

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import pygame
 from constants import *
+from player import * 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    X = SCREEN_WIDTH / 2 
+    Y = SCREEN_HEIGHT / 2 
+    player_1 = Player(X, Y)
      
     while True:
        for event in pygame.event.get():
            if event.type == pygame.QUIT:
                return
        screen.fill((0, 0, 0))
+       player_1.draw(screen)
        pygame.display.flip()
+       dt = (clock.tick(60)) / 1000
+
+    
 
     # print("Starting asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}")
